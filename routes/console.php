7b932e21bdff8e42ddfc612705227a53b23dc16f
KhadaYeah <pespinoza9494@utm.edu.ec>
2024-08-14 16:26:41 -0500

    Establecido el horario de notificaciones

--- routes/console.php
+++ routes/console.php
@@ -5,12 +5,6 @@ use Illuminate\Support\Facades\Artisan;
 
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command("inspire", function () {
-    $this->comment(Inspiring::quote());
-})
-    ->purpose("Display an inspiring quote")
-    ->hourly();
-
 Schedule::call(function () {
     \App\Http\Controllers\NotificationController::enviarNotificaciones();
-})->dailyAt("08:00");
+})->dailyAt("09:00");
